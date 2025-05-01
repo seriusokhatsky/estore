1d488bdf316c6879eb34eca3d4d419cd8767709a
Serg <seriu.1995@gmail.com>
2025-05-01 20:40:19 +0200

    make some error

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -11,16 +11,14 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard({ products }) {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
+    return ( 
+           <AppLayout breadcrumbs={breadcrumbs}> <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         {products.map(product => {
                             return <p>{product.name}</p>
-                        })}
-                    </div>
+                        })}</div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
