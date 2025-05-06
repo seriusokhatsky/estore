3cd6566a848fb22849dda573dd2c98d643e37d93
Serg <seriu.1995@gmail.com>
2025-05-06 08:22:02 +0200

    db query log

---
+++ app/Providers/DatabaseQueryLoggerServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
+
+class DatabaseQueryLoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        DB::listen(function ($query) {
+            // File::append(
+            //     storage_path('/logs/query.log'),
+            //     '[' . date('Y-m-d H:i:s') . ']' . PHP_EOL . $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL . PHP_EOL
+            // );
+        });
+    }
+}
